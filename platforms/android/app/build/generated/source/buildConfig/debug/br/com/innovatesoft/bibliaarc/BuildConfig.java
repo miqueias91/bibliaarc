507fca5ae4d766e9f60f797cdf53d31aa4c6b57a
Miqueias <miqueias.caetano@doctum.edu.br>
2021-05-12 18:36:23 -0300

    ok

--- platforms/android/app/build/generated/source/buildConfig/debug/br/com/innovatesoft/bibliaarc/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/br/com/innovatesoft/bibliaarc/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "br.com.innovatesoft.bibliaarc";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 10014;
-  public static final String VERSION_NAME = "1.0.14";
+  public static final int VERSION_CODE = 10015;
+  public static final String VERSION_NAME = "1.0.15";
 }
