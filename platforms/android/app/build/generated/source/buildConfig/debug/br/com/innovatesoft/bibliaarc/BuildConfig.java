cd95e664214df21b5bffa34eb7fc98eaa4fcb6ee
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-05-15 20:06:09 -0300

    ok

--- platforms/android/app/build/generated/source/buildConfig/debug/br/com/innovatesoft/bibliaarc/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/br/com/innovatesoft/bibliaarc/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "br.com.innovatesoft.bibliaarc";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 10015;
-  public static final String VERSION_NAME = "1.0.15";
+  public static final int VERSION_CODE = 10016;
+  public static final String VERSION_NAME = "1.0.16";
 }
